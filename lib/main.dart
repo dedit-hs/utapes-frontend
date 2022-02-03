165bc2d31011e018c0f5e6558f0d81fcf9e2c8a7
Dedit Hery Suprastyo <dedit@Dedits-iMac.local>
2022-02-03 07:09:54 +0700

    first init

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:utapes/pages/sign_in_page.dart';
+import 'package:utapes/pages/sign_up_page.dart';
+import 'package:utapes/pages/splash_page.dart';
+import 'theme.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => SplashPage(),
+        '/sign-in': (context) => SignInPage(),
+        '/sign-up': (context) => SignUpPage(),
+      },
+    );
+  }
+}
